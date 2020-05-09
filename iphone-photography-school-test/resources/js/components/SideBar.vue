0ed6cf5f01ed79f7ba5be64d8d57290e6f3c950e
Kofku <ewe727@gmail.com>
2020-05-09 21:27:02 +0100

    Created Basic Desktop Blog Page
    Created basic desktop blog page by creating two components
    resources/js/components/BlogPost and
    resources/js/components/SideBar so it can be reusable.
    Components get called in resources/js/app and
    resources/views/welcome.
    Added reusable general page styling in resources/sass/general-styling.
    Added blog post image in public/images/blog-image
    
     #2

---
+++ iphone-photography-school-test/resources/js/components/SideBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="SideBar">
+        <a class="button button--purplePink">
+            Send Me The Tips
+        </a>
+    </div>
+</template>
+
+<style>
+    .SideBar {
+        background: #F5F5F5;
+        display: inline-block;
+        height: 640px;
+        padding: 50px 24px 50px 26px ;
+        vertical-align: top;
+        width: 220px;
+    }
+</style>
